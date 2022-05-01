f623f9f8318654bfc4b0c9bd9eb86be13b060d28
Snehal1499 <snehalpatil1718@gmail.com>
2022-05-01 16:36:25 +0530

    Added UC8 Alter Table Query

---
+++ SQLQuery8.sql
@@ -0,0 +1,17 @@
+use payroll_service
+
+
+
+ALTER TABLE employee_payroll
+ADD Phone_Number VarChar(12)
+Go
+ALTER TABLE employee_payroll
+ADD departmen varchar(20)
+Go
+ALTER TABLE employee_payroll
+ADD emp_address varchar(20)
+Go
+
+select *
+from employee_payroll
+Go
