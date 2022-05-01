70f9b110d79f27b85524e559361c97336e89d650
Snehal1499 <snehalpatil1718@gmail.com>
2022-05-01 16:41:16 +0530

    Added UC9 Alter Pay Query

---
+++ SQLQuery9.sql
@@ -0,0 +1,21 @@
+use payroll_service
+
+ALTER TABLE employee_payroll
+ADD Basic_Pay Money
+Go
+ALTER TABLE employee_payroll
+ADD Deductions Money
+Go
+ALTER TABLE employee_payroll
+ADD Taxable_Pay Money
+Go
+ALTER TABLE employee_payroll
+ADD Income_Tax Money
+Go
+ALTER TABLE employee_payroll
+ADD Net_Pay Money
+Go
+
+select *
+from employee_payroll
+Go
