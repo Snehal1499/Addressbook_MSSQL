9393931bc88dec2ea42a9c966e92b5bbbc0d6f9d
Snehal1499 <snehalpatil1718@gmail.com>
2022-05-01 16:55:32 +0530

    Added UC10 multiple department

---
+++ SQLQuery10.sql
@@ -0,0 +1,14 @@
+use payroll_service
+
+Insert into employee_payroll 
+(Name,StartDate,Gender,Phone_Number,department,emp_address,Basic_Pay,Deductions,Taxable_Pay,Income_Tax,Net_Pay)
+values ('Terisa','2022-04-01','F',9090898977,'Sales','india',50000,2000,48000,3000,45000)
+Go
+Insert into employee_payroll 
+(Name,StartDate,Gender,Phone_Number,department,emp_address,Basic_Pay,Deductions,Taxable_Pay,Income_Tax,Net_Pay)
+values ('Terisa','2020-01-01','F',8990084977,'Marketing','india',80000,2000,78000,3000,75000)
+Go
+
+select *
+from employee_payroll
+Go
